e2ca4e70593ee5e2dab3b4939652672cfc24c5f5
mztikk <nevigskcuforez@gmail.com>
2021-01-05 00:48:25 +0100

    removed scripts and build from .gitignore

---
+++ build.ps1
@@ -0,0 +1,99 @@
+param (
+    [Parameter()]
+    [Alias("out")]
+    [string]
+    $output = "./out",
+
+    [Parameter()]
+    [string[]]
+    $modules,
+
+    [Parameter()]
+    [switch]
+    $allModules
+)
+
+$scriptTag = "Script"
+$fileAttrib = "file"
+
+$embeds = New-Object System.XML.XMLDocument
+# $embeds.Load("$($output)/embeds.xml")
+# <Ui xmlns="http://www.blizzard.com/wow/ui/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.blizzard.com/wow/ui/..\FrameXML\UI.xsd">
+
+$ui = $embeds.CreateElement("Ui")
+
+[void]$ui.SetAttribute("xmlns", "http://www.blizzard.com/wow/ui/")
+[void]$ui.SetAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance")
+[void]$ui.SetAttribute("xsi:schemaLocation", "http://www.blizzard.com/wow/ui/..\FrameXML\UI.xsd")
+[void]$embeds.AppendChild($ui)
+
+$libIncludes = @(
+    "Libs/LibStub/LibStub.lua",
+    "Libs/LibRangeCheck-2.0/LibRangeCheck-2.0.lua",
+    "Libs/LibSharedMedia-3.0/LibSharedMedia-3.0.lua"
+)
+
+$baseIncludes = @(
+    "base/Init.lua",
+    "helpers/TableHelpers.lua",
+    "helpers/OutputHelpers.lua",
+    "modules/units/UnitInfos.lua",
+    "base/Blade.lua",
+    "commands/ChatCommands.lua",
+    "events/EventHandlers.lua",
+    "base/Startup.lua"
+)
+
+$moduleIncludes = @{
+    auralogger  = @("modules/extra/auralogger.lua")
+    spelllogger = @("modules/extra/spelllogger.lua")
+}
+
+function AddFile([string]$file) {
+    $child = $embeds.CreateElement($scriptTag)
+    [void]$child.SetAttribute($fileAttrib, $file)
+    [void]$ui.AppendChild($child)
+}
+
+function CreateOutputFile([string]$file) {
+    $dest = "$($output)/$($file)"
+    New-Item $dest -Force
+    Copy-Item "src/$($file)" $dest -Force
+}
+
+If ((Test-Path $output)) {
+    Remove-Item -Recurse -Force $output
+}
+
+New-Item -ItemType Directory -Force -Path $output
+
+foreach ($item in $libIncludes) {
+    AddFile $item
+    CreateOutputFile $item
+}
+
+foreach ($item in $baseIncludes) {
+    AddFile $item
+    CreateOutputFile $item
+}
+
+if ($allModules) {
+    foreach ($module in $moduleIncludes) {
+        foreach ($item in $module.Values) {
+            AddFile $item
+            CreateOutputFile $item
+        }
+    }
+}
+else {
+    foreach ($module in $modules) {
+        foreach ($item in $moduleIncludes[$module]) {
+            AddFile $item
+            CreateOutputFile $item
+        }
+    }
+}
+
+CreateOutputFile "Blade.toc"
+
+$embeds.Save("$($output)/embeds.xml")
---
+++ scripts/publish.ps1
@@ -0,0 +1,5 @@
+Push-Location ..
+./build.ps1 -allModules
+
+E:/repos/DiffCopy/build/DiffCopy.exe "out" "C:\Games\World of Warcraft\_retail_\Interface\AddOns\Blade"
+Pop-Location
