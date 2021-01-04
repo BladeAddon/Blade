d3f244ad4aaa9663704fcfcaa8a7671afc0b8fd2
mztikk <nevigskcuforez@gmail.com>
2021-01-05 00:51:29 +0100

    changed addon color to deep pink

--- src/helpers/OutputHelpers.lua
+++ src/helpers/OutputHelpers.lua
@@ -1,8 +1,11 @@
 local ns, BLADE = ...
 
 local COBALT_BLUE = "|cFF0047AB"
+local DEEP_PINK = "|cFFff1493"
 local WHITE = "|cFFFFFFFF"
 local RETURN_COLOR = "|r"
+
+local ADDON_COLOR = DEEP_PINK
 
 local print = print
 
@@ -19,7 +22,7 @@ end
 
 local function GetPrintPrefix()
     return BLADE:EncodeInColor("[", WHITE) ..
-        BLADE:EncodeInColor(BLADE.AddonName, COBALT_BLUE) .. BLADE:EncodeInColor("]", WHITE)
+        BLADE:EncodeInColor(BLADE.AddonName, ADDON_COLOR) .. BLADE:EncodeInColor("]", WHITE)
 end
 
 function BLADE:Print(...)
