5c63df848e479b8d6c5e103ce7300e52f147c9df
mztikk <nevigskcuforez@gmail.com>
2021-01-05 10:55:54 +0100

    chat commands can now have description, added help display when no command is entered

--- src/commands/ChatCommands.lua
+++ src/commands/ChatCommands.lua
@@ -1,11 +1,28 @@
 local ns, Blade = ...
 Blade.commands = {}
 
+local function ChatCommand(name, description, handler)
+    return {Name = name, Description = description, Handler = handler}
+end
+
+local function DisplayHelp()
+    for commandName, commands in pairs(Blade.commands) do
+        for _, command in ipairs(commands) do
+            Blade:Print(commandName, command.Description and command.Description or "")
+        end
+    end
+end
+
 SLASH_Blade1 = "/blade"
 SLASH_Blade2 = "/bla"
 SLASH_Blade3 = "/bl"
 SlashCmdList["Blade"] = function(msg)
     local cmd, param, param2 = msg:match("^([%w%-]+)%s*([^%s]*)%s*(.*)$")
+
+    if not cmd then
+        DisplayHelp()
+        return
+    end
 
     cmd = cmd or ""
     param = param or ""
@@ -13,24 +30,20 @@ SlashCmdList["Blade"] = function(msg)
 
     local matchingCommand = false
 
-    for k, v in pairs(Blade.commands) do
-        if cmd == k then
-            matchingCommand = true
-            for i = 1, #v do
-                v[i](param, param2)
-            end
+    if Blade.commands[cmd] then
+        local v = Blade.commands[cmd]
+        for i = 1, #v do
+            v[i].Handler(param, param2)
         end
-    end
-
-    if not matchingCommand then
+    else
         Blade:Print("No matching command found for: '" .. cmd .. "'")
     end
 end
 
-function Blade:RegisterCommand(command, handler)
+function Blade:RegisterCommand(command, handler, description)
     if not self.commands[command] then
         self.commands[command] = {}
     end
 
-    table.insert(self.commands[command], handler)
+    table.insert(self.commands[command], ChatCommand(command, description, handler))
 end
