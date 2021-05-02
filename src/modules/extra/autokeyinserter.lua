a5c85beed3828c5b178e96ebf7f8cb5586704acb
Nils Feierabend <mztikk@outlook.de>
2021-05-02 22:19:17 +0200

    respect ENABLED setting for AUTOKEYINSERTER

--- src/modules/extra/autokeyinserter.lua
+++ src/modules/extra/autokeyinserter.lua
@@ -7,6 +7,10 @@ local keystone_item_id = 180653
 Blade:RegisterEvent(
     "CHALLENGE_MODE_KEYSTONE_RECEPTABLE_OPEN",
     function(...)
+        if not Blade:GetSetting(moduleName, "ENABLED") then
+            return
+        end
+
         local bag, slot = Blade:FindBagItemByID(keystone_item_id)
         if bag and slot then
             UseContainerItem(bag, slot)
