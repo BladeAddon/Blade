351be470d4d6dbe201d34ad8f2f056fb79c2435e
mztikk <mztikk@outlook.de>
2022-11-13 20:34:39 +0100

    added ContainerItem api

--- declaration/Container.d.ts
+++ declaration/Container.d.ts
@@ -3,9 +3,13 @@ declare type BAG_ID = -4 | -2 | -1 | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 
 declare const NUM_BAG_SLOTS: number
 /** @noSelf **/
 declare function GetContainerNumSlots(bag_id: BAG_ID): number
+
+declare type ContainerItemInfoResult = [texture: string, itemCount: number, locked: boolean, quality: number, readable: boolean, lootable: boolean, itemLink: string, isFiltered: boolean, noValue: boolean, itemID: number, isBound: boolean]
 /** @noSelf **/
-declare function GetContainerItemInfo(bag_id: BAG_ID, slot: number): LuaMultiReturn<[texture: string, itemCount: number, locked: boolean, quality: number, readable: boolean, lootable: boolean, itemLink: string, isFiltered: boolean, noValue: boolean, itemID: number, isBound: boolean]>
+declare function GetContainerItemInfo(bag_id: BAG_ID, slot: number): LuaMultiReturn<ContainerItemInfoResult>
+
+declare type ItemInfoResult = [itemName: string, itemLink: string, itemRarity: number, itemLevel: number, itemMinLevel: number, itemType: string, itemSubType: string, ItemStackCount: number, itemEquipLoc: string, itemTexture: string, itemSellPrice: number]
 /** @noSelf **/
-declare function GetItemInfo(item_id: number): LuaMultiReturn<[itemName: string, itemLink: string, itemRarity: number, itemLevel: number, itemMinLevel: number, itemType: string, itemSubType: string, ItemStackCount: number, itemEquipLoc: string, itemTexture: string, itemSellPrice: number]>
+declare function GetItemInfo(item_id: number): LuaMultiReturn<ItemInfoResult>
 /** @noSelf **/
-declare function UseContainerItem(bag_id: BAG_ID, slot: number)
+declare function UseContainerItem(bag_id: BAG_ID, slot: number): void
---
+++ src/api/ContainerItem.ts
@@ -0,0 +1,88 @@
+export class ContainerItem {
+    private _containerItemInfo: ContainerItemInfoResult | undefined
+    private _itemInfo: ItemInfoResult | undefined
+    constructor(public readonly bag_id: BAG_ID, public readonly slot: number) {
+    }
+
+    public get containerItemInfo(): ContainerItemInfoResult {
+        return this._containerItemInfo ??= GetContainerItemInfo(this.bag_id, this.slot)
+    }
+
+    /**The icon texture (FileID) for the item in the specified bag slot. */
+    public get texture(): string {
+        return this.containerItemInfo[0]
+    }
+
+    /**The number of items in the specified bag slot. */
+    public get itemCount(): number {
+        return this.containerItemInfo[1]
+    }
+
+    /**True if the item is locked by the server, false otherwise. */
+    public get locked(): boolean {
+        return this.containerItemInfo[2]
+    }
+
+    /**The Quality of the item. */
+    public get quality(): number {
+        return this.containerItemInfo[3]
+    }
+
+    /**True if the item can be "read" (as in a book), false otherwise. */
+    public get readable(): boolean {
+        return this.containerItemInfo[4]
+    }
+
+    /**True if the item is a temporary container containing items that can be looted, false otherwise. */
+    public get lootable(): boolean {
+        return this.containerItemInfo[5]
+    }
+
+    /**The itemLink of the item in the specified bag slot. */
+    public get itemLink(): string {
+        return this.containerItemInfo[6]
+    }
+
+    /**True if the item is grayed-out during the current inventory search, false otherwise. */
+    public get isFiltered(): boolean {
+        return this.containerItemInfo[7]
+    }
+
+    /**True if the item has no gold value, false otherwise. */
+    public get noValue(): boolean {
+        return this.containerItemInfo[8]
+    }
+
+    /**The unique ID for the item in the specified bag slot. */
+    public get itemID(): number {
+        return this.containerItemInfo[9]
+    }
+
+    /**True if the item is bound to the current character, false otherwise. */
+    public get isBound(): boolean {
+        return this.containerItemInfo[10]
+    }
+
+    public get itemInfo(): ItemInfoResult {
+        return this._itemInfo ??= GetItemInfo(this.itemID)
+    }
+
+    /**The localized name of the item. */
+    public get name(): string {
+        return this.itemInfo[0]
+    }
+
+    /**The base item level, not including upgrades. See GetDetailedItemLevelInfo() for getting the actual item level. */
+    public get itemLevel(): number {
+        return this.itemInfo[3]
+    }
+
+    /**The vendor price in copper, or 0 for items that cannot be sold. */
+    public get sellPrice(): number {
+        return this.itemInfo[10]
+    }
+
+    public Use(): void {
+        UseContainerItem(this.bag_id, this.slot)
+    }
+}
