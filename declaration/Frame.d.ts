19b024665249a6fa78259743c7d4665eebb75802
mztikk <mztikk@outlook.de>
2022-11-13 20:34:55 +0100

    fix frame decls

--- declaration/Frame.d.ts
+++ declaration/Frame.d.ts
@@ -1,11 +1,11 @@
 declare type WidgetHandler = "OnUpdate"
 
 declare interface Frame {
-    HookScript(event: WidgetHandler, handler: (frame: T, ...args: any[]) => void): void
-    HookScript(event: "OnUpdate", handler: (frame: T, elapsed: number) => void): void
+    HookScript(event: WidgetHandler, handler: (frame: Frame, ...args: any[]) => void): void
+    HookScript(event: "OnUpdate", handler: (frame: Frame, elapsed: number) => void): void
     RegisterEvent(event: string)
     SetScript<T extends any[]>(event: string, handler: (T) => void)
-    SetScript(event: "OnEvent", handler: (frame: T, eventName: string, ...args: any[]) => void): void;
+    SetScript(event: "OnEvent", handler: (frame: Frame, eventName: string, ...args: any[]) => void): void
 }
 
 /** @noSelf **/
