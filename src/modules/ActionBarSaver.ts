211e792cd1d6b8bc0d3d0576ad7b83073a0c3c8e
mztikk <mztikk@outlook.de>
2022-11-25 17:23:48 +0100

    ActionBarSaver refactoring

--- src/modules/ActionBarSaver.ts
+++ src/modules/ActionBarSaver.ts
@@ -1,7 +1,129 @@
 import { ChatCommand } from '../api/ChatCommand'
 import { CommandHandler } from '../api/CommandHandler'
+import { ConfigService } from '../ConfigService'
 import { Inject } from '../tstl-di/src/Inject'
 import { Module } from './Module'
+
+class ActionBarLoader {
+    private readonly _macros: ConfigService
+    private readonly _actions: ConfigService
+    constructor(private readonly _db: ConfigService) {
+        this._macros = this._db.GetConfig("macros")
+        this._actions = this._db.GetConfig("actions")
+    }
+
+    private ClearActionSlot(slot: number): void {
+        const [actionType, _id, _subType] = GetActionInfo(slot)
+        if (actionType !== undefined) {
+            PickupAction(slot)
+            ClearCursor()
+        }
+    }
+
+    private FindMacro(macro: Macro): number | undefined {
+        for (let index = 0; index < 999; index++) {
+            const [name, _icon, body] = GetMacroInfo(index)
+            if (name !== undefined) {
+                if (name === macro.name && body === macro.body) {
+                    return index
+                }
+            }
+        }
+
+        return undefined
+    }
+
+    private FindSpellBook(id: number): { index: number, bookType: BookType } | undefined {
+        for (let spellTab = 1; spellTab <= GetNumSpellTabs(); spellTab++) {
+            const [_name, _texture, offset, numSlots, _isGuild, _offspecID] = GetSpellTabInfo(spellTab)
+            for (let index = offset + 1; index <= offset + numSlots; index++) {
+                const [_spellType, spellBookItemID] = GetSpellBookItemInfo(index, BOOKTYPE_SPELL)
+                if (id === spellBookItemID) {
+                    return {
+                        index: index,
+                        bookType: BOOKTYPE_SPELL
+                    }
+                }
+            }
+        }
+
+        return undefined
+    }
+
+    private LoadMacro(slot: number, action: Action): void {
+        const macro = this._macros.Get<Macro>(action.id)
+        if (!macro) {
+            error(`macro ${action.id} for slot ${slot} was saved doesn't exist`)
+        }
+
+        const [name, icon, body] = GetMacroInfo(action.id)
+        if (name === macro.name && icon === macro.icon && body == macro.body) {
+            PickupMacro(macro.id)
+        } else {
+            const macroID = this.FindMacro(macro) ?? CreateMacro(macro.name, macro.icon, macro.body, true)
+            PickupMacro(macroID)
+        }
+
+        PlaceAction(slot)
+    }
+
+    private LoadMount(slot: number, action: Action): void {
+        // random favorite
+        if (action.id === 268435455) {
+            C_MountJournal.Pickup(0)
+        } else {
+            // find displayIndex
+            for (let index = 1; index <= C_MountJournal.GetNumMounts(); index++) {
+                if (C_MountJournal.GetDisplayedMountID(index) === action.id) {
+                    C_MountJournal.Pickup(index)
+                    break
+                }
+            }
+        }
+
+        PlaceAction(slot)
+    }
+
+    private LoadFlyout(slot: number, action: Action): void {
+        const spellbookItem = this.FindSpellBook(action.id)
+        if (spellbookItem) {
+            PickupSpellBookItem(spellbookItem.index, spellbookItem.bookType)
+            PlaceAction(slot)
+        }
+    }
+
+    private LoadAction(slot: number, action: Action): void {
+        if (action.type === "macro") {
+            this.LoadMacro(slot, action)
+        } else if (action.type === "spell") {
+            PickupSpell(action.id)
+            PlaceAction(slot)
+        } else if (action.type === "item") {
+            PickupItem(action.id)
+            PlaceAction(slot)
+        } else if (action.type === "summonmount") {
+            this.LoadMount(slot, action)
+        } else if (action.type === "flyout") {
+            this.LoadFlyout(slot, action)
+        }
+
+        ClearCursor()
+    }
+
+    public Execute(): void {
+        ClearCursor()
+
+        for (let slot = 1; slot <= 512; slot++) {
+            this.ClearActionSlot(slot)
+
+            const action = this._actions.Get<Action>(slot)
+            if (action) {
+                this.LoadAction(slot, action)
+            }
+        }
+    }
+}
+
 export class ActionBarSaver extends Module {
     @Inject("CommandHandler") private readonly _commandHandler!: CommandHandler
 
@@ -56,102 +178,9 @@ export class ActionBarSaver extends Module {
         this._output.Print(this._localization.Format("SAVED_PROFILE", profile))
     }
 
-    private FindMacro(macro: Macro): number | undefined {
-        for (let index = 0; index < 999; index++) {
-            const [name, _icon, body] = GetMacroInfo(index)
-            if (name !== undefined) {
-                if (name === macro.name && body === macro.body) {
-                    return index
-                }
-            }
-        }
-
-        return undefined
-    }
-
-    private ClearActionSlot(slot: number): void {
-        const [actionType, _id, _subType] = GetActionInfo(slot)
-        if (actionType !== undefined) {
-            PickupAction(slot)
-            ClearCursor()
-        }
-    }
-
-    private FindSpellBook(id: number): { index: number, bookType: BookType } | undefined {
-        for (let spellTab = 1; spellTab <= GetNumSpellTabs(); spellTab++) {
-            const [_name, _texture, offset, numSlots, _isGuild, _offspecID] = GetSpellTabInfo(spellTab)
-            for (let index = offset + 1; index <= offset + numSlots; index++) {
-                const [_spellType, spellBookItemID] = GetSpellBookItemInfo(index, BOOKTYPE_SPELL)
-                if (id === spellBookItemID) {
-                    return {
-                        index: index,
-                        bookType: BOOKTYPE_SPELL
-                    }
-                }
-            }
-        }
-
-        return undefined
-    }
-
     private LoadProfile(profile: string): void {
-        ClearCursor()
         const db = this._db.GetConfig(profile)
-        const macros = db.GetConfig("macros")
-        const actions = db.GetConfig("actions")
-        for (let slot = 1; slot <= 512; slot++) {
-            this.ClearActionSlot(slot)
-
-            const action = actions.Get<Action>(slot)
-            if (action) {
-                if (action.type === "macro") {
-                    const macro = macros.Get<Macro>(action.id)
-                    if (!macro) {
-                        error(`macro ${action.id} for slot ${slot} was saved doesn't exist`)
-                    }
-
-                    const [name, icon, body] = GetMacroInfo(action.id)
-                    if (name === macro.name && icon === macro.icon && body == macro.body) {
-                        PickupMacro(macro.id)
-                    } else {
-                        const macroID = this.FindMacro(macro) ?? CreateMacro(macro.name, macro.icon, macro.body, true)
-                        PickupMacro(macroID)
-                    }
-
-                    PlaceAction(slot)
-                } else if (action.type === "spell") {
-                    PickupSpell(action.id)
-                    PlaceAction(slot)
-                } else if (action.type === "item") {
-                    PickupItem(action.id)
-                    PlaceAction(slot)
-                } else if (action.type === "summonmount") {
-                    // random favorite
-                    if (action.id === 268435455) {
-                        C_MountJournal.Pickup(0)
-                    } else {
-                        // find displayIndex
-                        for (let index = 1; index <= C_MountJournal.GetNumMounts(); index++) {
-                            if (C_MountJournal.GetDisplayedMountID(index) === action.id) {
-                                C_MountJournal.Pickup(index)
-                                break
-                            }
-                        }
-                    }
-
-                    PlaceAction(slot)
-                } else if (action.type === "flyout") {
-                    const spellbookItem = this.FindSpellBook(action.id)
-                    if (spellbookItem) {
-                        PickupSpellBookItem(spellbookItem.index, spellbookItem.bookType)
-                        PlaceAction(slot)
-                    }
-                }
-
-                ClearCursor()
-            }
-        }
-
+        new ActionBarLoader(db).Execute()
         this._output.Print(this._localization.Format("LOADED_PROFILE", profile))
     }
 }
