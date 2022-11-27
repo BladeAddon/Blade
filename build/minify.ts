ea96674d6716a10beeaf63ebf69aed56fdf6bf73
mztikk <mztikk@outlook.de>
2022-11-27 22:13:35 +0100

    minify deno native

--- build/minify.ts
+++ build/minify.ts
@@ -1,6 +1,5 @@
 import * as fs from "https://deno.land/std@0.163.0/fs/mod.ts"
 import * as path from "https://deno.land/std@0.163.0/path/mod.ts"
-import $ from "https://deno.land/x/dax@0.15.0/mod.ts";
 
 const addon_name = "Blade"
 const out_directory = "./out"
@@ -8,7 +7,6 @@ const out_directory_full = path.resolve(out_directory)
 const out_path = path.join(out_directory_full, `${addon_name}.lua`)
 const min_path = path.join(out_directory_full, `${addon_name}.min.lua`)
 
-const minified = await $`npx luamin -f ${out_path}`.text()
-await Deno.writeTextFile(min_path, minified);
+await Deno.run({ cmd: [`npx luamin -f ${out_path} > ${min_path}`] }).status()
 
 await fs.move(min_path, out_path, { overwrite: true })
