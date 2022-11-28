f852b66c492ef2507d6497153bd3606f8da670a3
mztikk <mztikk@outlook.de>
2022-11-28 01:45:02 +0100

    minify all lua files in out

--- build/minify.ts
+++ build/minify.ts
@@ -1,16 +1,29 @@
 import * as fs from "https://deno.land/std@0.163.0/fs/mod.ts"
 import * as path from "https://deno.land/std@0.163.0/path/mod.ts"
 
-const addon_name = "Blade"
+console.log(`running on ${Deno.build.os}`)
+
 const out_directory = "./out"
 const out_directory_full = path.resolve(out_directory)
-const out_path = path.join(out_directory_full, `${addon_name}.lua`)
-const min_path = path.join(out_directory_full, `${addon_name}.min.lua`)
 
-if (Deno.build.os === "windows") {
-    await Deno.run({ cmd: ["cmd", "/c", `npx luamin -f ${out_path} > ${min_path}`] }).status()
-} else if (Deno.build.os === "linux") {
-    await Deno.run({ cmd: ["bash", "-c", `npx luamin -f ${out_path} > ${min_path}`] }).status()
+const tasks: Promise<void>[] = []
+
+async function minify(path: string): Promise<void> {
+    console.log(`minifying ${path}`)
+    if (Deno.build.os === "windows") {
+        const output = await Deno.run({ cmd: ["cmd", "/c", "npx", "luamin", "-f", path], stdout: "piped" }).output()
+        const outStr = new TextDecoder().decode(output)
+        await Deno.writeTextFile(path, outStr)
+    } else if (Deno.build.os === "linux") {
+        const output = await Deno.run({ cmd: ["bash", "-c", "npx", "luamin", "-f", path] }).output()
+        const outStr = new TextDecoder().decode(output)
+        await Deno.writeTextFile(path, outStr)
+    }
 }
 
-await fs.move(min_path, out_path, { overwrite: true })
+for await (const entry of fs.walk(out_directory_full, { exts: [".lua"] })) {
+    const file_path_full = path.resolve(entry.path)
+    tasks.push(minify(file_path_full))
+}
+
+await Promise.all(tasks)
