5709f2733b396f975410089c2cafcce160a13b1e
mztikk <mztikk@outlook.de>
2022-12-17 11:34:06 +0100

    added constant KEYSTONE_ITEM_ID to Items

--- src/item/Items.ts
+++ src/item/Items.ts
@@ -1,4 +1,6 @@
 export class Items {
+    public static readonly KEYSTONE_ITEM_ID = 180653
+
     public LoadItems(itemIDs: number[], handler: () => void): void {
         const items = new Set<number>(itemIDs)
         const numToLoad = items.size
