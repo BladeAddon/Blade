ad381e2a9aa6b4ab5ec1d525e99b798f072c5908
mztikk <mztikk@outlook.de>
2022-12-20 18:50:50 +0100

    added cacheless bag impl for testing purposes

--- src/item/bag/Bag.ts
+++ src/item/bag/Bag.ts
@@ -1,53 +1,27 @@
-import { IEventHandler } from '../../event/IEventHandler'
 import { Loadable } from '../../loadable/Loadable'
-import { Inject } from '../../tstl-di/src/Inject'
 import { ContainerItem } from './ContainerItem'
 
 declare type ContainerItemFunction = (item: ContainerItem) => void
 declare type ContainerItemPredicate = (item: ContainerItem) => boolean
 
 export class Bag extends Loadable {
-    @Inject("IEventHandler") private readonly _eventHandler!: IEventHandler
-
     constructor() {
         super("Bag")
     }
 
-    private readonly _containerLookup: LuaMap<number, LuaMap<number, ContainerItem>> = new LuaMap()
-
-    private UpdateBags(): void {
-        for (let bagIndex = 0; bagIndex <= NUM_BAG_SLOTS; bagIndex++) {
-            this.UpdateBag(bagIndex)
-        }
-    }
-
-    private UpdateBag(bagIndex: number): void {
-        const bag: LuaMap<number, ContainerItem> = new LuaMap()
-        this._containerLookup.set(bagIndex, bag)
-
-        for (let slotIndex = 1; slotIndex <= C_Container.GetContainerNumSlots(bagIndex); slotIndex++) {
-            const containerItem = ContainerItem.Create(bagIndex, slotIndex)
-            if (containerItem) {
-                bag.set(slotIndex, containerItem)
-            }
-        }
-    }
-
     protected OnLoad(): void {
-        this.UpdateBags()
-        this._eventHandler.RegisterEvent("BAG_UPDATE", this.UpdateBag.bind(this))
+        return
     }
 
     public *IterItems() {
-        for (const [_, bag] of this._containerLookup) {
-            for (const [_, item] of bag) {
-                yield item
+        for (let bagIndex = 0; bagIndex <= NUM_BAG_SLOTS; bagIndex++) {
+            for (let slotIndex = 1; slotIndex <= C_Container.GetContainerNumSlots(bagIndex); slotIndex++) {
+                const containerItem = ContainerItem.Create(bagIndex, slotIndex)
+                if (containerItem) {
+                    yield containerItem
+                }
             }
         }
-    }
-
-    public GetContainerItem(bagIndex: number, slotIndex: number): ContainerItem | undefined {
-        return this._containerLookup.get(bagIndex)?.get(slotIndex)
     }
 
     public FindBagItemByID(itemID: number): ContainerItem | undefined {
